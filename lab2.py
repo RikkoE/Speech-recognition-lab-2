6a6da09d00288cbfcd622ee0ff04ae5de5040d51
Rikko <rickard.e.1992@gmail.com>
2016-04-19 14:50:26 +0200

    first commit
    first commit

---
+++ lab2.py
@@ -0,0 +1,8 @@
+import proto2 as pro
+import matplotlib.pyplot as plt
+import numpy as np 
+import tools2 as tool
+
+
+example = np.load('lab2_example.npz')['example'].item()
+tidigits = np.load('lab2_tidigits.npz')['tidigits']
