583467cdb2ef7d359c2fdbc9de9f7882af044c46
Rikko <rickard.e.1992@gmail.com>
2016-04-20 15:35:33 +0200

    GMM and HMM first part done
    GMM and HMM first part done

--- lab2.py
+++ lab2.py
@@ -2,15 +2,39 @@ import proto2 as pro
 import matplotlib.pyplot as plt
 import numpy as np 
 import tools2 as tool
+from sklearn import mixture
 
 
 example = np.load('lab2_example.npz')['example'].item()
 tidigits = np.load('lab2_tidigits.npz')['tidigits']
 models = np.load('lab2_models.npz')['models']
 
-test = example['mfcc']
+exmfcc = example['mfcc']
+exhmmobs = example['hmm_obsloglik']
+exgmmobs = example['gmm_obsloglik']
 
-plt.imshow(test.T, interpolation = 'nearest', aspect = 'auto', origin = 'lower')
-plt.colorbar()
+modelHmm = models[0]['hmm']
+modkeyHmm = modelHmm.keys()
+
+cvHmm = modelHmm['covars']
+muHmm = modelHmm['means']
+
+hmmobs = mixture.log_multivariate_normal_density(exmfcc, muHmm, cvHmm, 'diag')
+
+
+modelGmm = models[0]['gmm']
+modkeyGmm = modelGmm.keys()
+
+cvGmm = modelGmm['covars']
+muGmm = modelGmm['means']
+
+gmmobs = mixture.log_multivariate_normal_density(exmfcc, muGmm, cvGmm, 'diag')
+
+
+ax = plt.subplot(2, 1, 1)
+ax.imshow(exgmmobs.T, interpolation = 'nearest', aspect = 'auto', origin = 'lower')
+
+ax = plt.subplot(2, 1, 2)
+ax.imshow(gmmobs.T, interpolation = 'nearest', aspect = 'auto', origin = 'lower')
 
 plt.show()
