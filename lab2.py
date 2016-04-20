5233dea8a6dd748ed00698902bbe6a3ea3464f2b
Rikko <rickard.e.1992@gmail.com>
2016-04-20 09:35:09 +0200

    Added stuff
    Added stuff

--- lab2.py
+++ lab2.py
@@ -6,3 +6,11 @@ import tools2 as tool
 
 example = np.load('lab2_example.npz')['example'].item()
 tidigits = np.load('lab2_tidigits.npz')['tidigits']
+models = np.load('lab2_models.npz')['models']
+
+test = example['mfcc']
+
+plt.imshow(test.T, interpolation = 'nearest', aspect = 'auto', origin = 'lower')
+plt.colorbar()
+
+plt.show()
